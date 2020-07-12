ac108040da01059e84b297eed4e4ef41999c4bf5
Igor Tuag <igortuag@gmail.com>
2020-07-12 15:39:09 -0300

    Criando projeto

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: Colors.black,
+  },
+});
+
+const App: () => React$Node = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
