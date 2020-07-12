d53703661b908445bc1a8d67b114be33e234d0dd
Igor Tuag <igortuag@gmail.com>
2020-07-12 16:43:56 -0300

    React Navigation

--- src/index.js
+++ src/index.js
@@ -1,31 +1,14 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
 import './config/ReactotronConfig';
-
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color: Colors.black,
-  },
-});
-
-console.tron.log('Hello World');
+import Routes from './routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Hello World</Text>
-    </View>
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
   );
 }
